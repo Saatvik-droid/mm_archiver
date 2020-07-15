a0d29a107500930699a2083a10788ca0e4fad090
SaatvikRamani <saatvikramani777@gmail.com>
2020-07-15 12:33:25 +0530

    fixed bugs and cleaned up

--- mm_archiver.py
+++ mm_archiver.py
@@ -1,25 +1,30 @@
 import praw
 
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 from secrets import secrets
 
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-p", dest="password", help="password for reddit account", required=True)
-parser.add_argument("-s", dest="subreddit", help="subreddit whose modmails have to be archived(enter multiple subreddits by adding a '+' between each)", required=True)
-parser.add_argument("-t", dest="time", help="time old modmails to be archived(in days)", default=0)
-parser.add_argument("-n", dest="number", help="maximum number mails to be archived, default", type=int, default=9999)
-parser.add_argument("-r", dest="reply", help="reply message if needed", default=None)
-args = parser.parse_args()
-
 # assign args to vars
-password = args.password
-subreddit = args.subreddit
-time = args.time
-number = args.number
-reply = args.reply
+subreddit = input("subreddit whose modmails have to be archived(enter multiple subreddits by adding a '+' between each)")
+time = input("time old modmails to be archived(in days)")
+number = input("maximum number mails to be archived")
+reply = input("reply message if needed")
+password = input("password for reddit account")
+
+if time.lower() == "none":
+    time = int(0)
+else:
+    time = int(time)
+
+if number.lower() == "none":
+    number = int(9999)
+else:
+    number = int(number)
+
+if reply.lower() == "none":
+    reply = None
+else:
+    reply = str(reply)
 
 # log into reddit
 reddit = praw.Reddit(client_id=secrets["client_id"],
@@ -29,30 +34,32 @@ reddit = praw.Reddit(client_id=secrets["client_id"],
                      username="SaatvikRamani")
 
 # obtain all modmail conversations
-modmails = reddit.subreddit(subreddit).modmail.conversations(sort="recent", state="all")
+modmails = reddit.subreddit(subreddit).modmail.conversations(other_subreddits=None, limit=1000, sort="recent", state="all")
 
 # counter to count number of modmails gone through
 counter = 0
 
+days_before = datetime.now(timezone.utc) - timedelta(days=time)
+
 # actual archiving feature
 for modmail in modmails:
     counter = counter + 1
+    print(modmail.owner.display_name)
+    print(counter)
 
     if counter > number:
         break
 
-    if reply is not None:
-        print("reply")
-        modmail.reply(reply, author_hidden=True)
-    else:
-        pass
-    if int((datetime.now(timezone.utc) - datetime.fromisoformat(modmail.last_updated)).days) >= int(time):
-        print("archive")
+    if datetime.fromisoformat(modmail.last_updated) < days_before:
+        if reply is not None:
+            modmail.reply(reply, author_hidden=True)
+        else:
+            pass
         modmail.reply("Auto archiving, someone ran the script", internal=True)
         modmail.archive()
     else:
         pass
+    print(f"{counter} modmail(s) archived")
 
-    print("{0} modmail(s) archived".format(counter))
 
 print("Successful!!!")
